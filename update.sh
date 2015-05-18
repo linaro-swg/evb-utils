1127dbcdc76e45871ddb9f40f59ff684f6b85a20
James Kung <James.Kung@mediatek.com>
2015-05-18 13:41:25 +0800

    OP-TEE support

--- update.sh
+++ update.sh
@@ -3,6 +3,6 @@ if ! fastboot flash UBOOT lk.bin ; then
 	fastboot flash lk lk.bin
 fi
 fastboot flash boot0 preloader.img
-fastboot flash TEE1 trustzone.bin 
-fastboot flash TEE2 trustzone.bin 
+fastboot flash TEE1 tz.img 
+fastboot flash TEE2 tz.img
 fastboot flash boot boot.img
